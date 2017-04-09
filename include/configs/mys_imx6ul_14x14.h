ca7b81f7ae323e25b8d435033a56d038a2560d78
KevinChen <kevin.chen@myirtech.com>
2017-04-09 17:53:30 +0800

    repartition all nand flash
    uboot 5MB
    kernel 10MB
    dtb 1MB

--- include/configs/mys_imx6ul_14x14.h
+++ include/configs/mys_imx6ul_14x14.h
@@ -93,7 +93,7 @@
 #define CONFIG_SYS_MMC_IMG_LOAD_PART	1
 
 #ifdef CONFIG_SYS_BOOT_NAND
-#define CONFIG_MFG_NAND_PARTITION "mtdparts=gpmi-nand:64m(boot),16m(kernel),16m(dtb),-(rootfs) "
+#define CONFIG_MFG_NAND_PARTITION "mtdparts=gpmi-nand:5m(boot),10m(kernel),1m(dtb),-(rootfs) "
 #else
 #define CONFIG_MFG_NAND_PARTITION ""
 #endif
@@ -123,9 +123,9 @@
 	"bootargs=console=ttymxc0,115200 ubi.mtd=3 "  \
 		"root=ubi0:rootfs rootfstype=ubifs "		     \
 		CONFIG_BOOTARGS_CMA_SIZE \
-		"mtdparts=gpmi-nand:64m(boot),16m(kernel),16m(dtb),-(rootfs)\0"\
-	"bootcmd=nand read ${loadaddr} 0x4000000 0x800000;"\
-		"nand read ${fdt_addr} 0x5000000 0x100000;"\
+		"mtdparts=gpmi-nand:5m(boot),10m(kernel),1m(dtb),-(rootfs)\0"\
+	"bootcmd=nand read ${loadaddr} 0x500000 0xA00000;"\
+		"nand read ${fdt_addr} 0xF00000 0x100000;"\
 		"bootz ${loadaddr} - ${fdt_addr}\0"
 
 #else
