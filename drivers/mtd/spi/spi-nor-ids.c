dd2daeac0ed4ed9e64ecc2dbdc917e93028ba453
duxy <568988005@qq.com>
2021-09-16 10:34:58 +0800

    FIX: giga 25lq256 support

--- drivers/mtd/spi/spi-nor-ids.c
+++ drivers/mtd/spi/spi-nor-ids.c
@@ -123,7 +123,7 @@ const struct flash_info spi_nor_ids[] = {
 			SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB)
 	},
 	{
-		INFO("gd25lq256", 0xc86019, 0, 64 * 1024, 256,
+		INFO("gd25lq256", 0xc86019, 0, 64 * 1024, 512,
 			SECT_4K | SPI_NOR_DUAL_READ |
 			SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB)
 	},
