8a2bab01c2f40cb47002f706cbae2339c8bf7783
wjl <1053924360@qq.com>
2022-08-14 13:40:20 +0800

    FIX:modify phy reset control

--- board/myir/myd_jx8mma7/myd_jx8mma7.c
+++ board/myir/myd_jx8mma7/myd_jx8mma7.c
@@ -21,6 +21,7 @@
 #include <usb.h>
 #include <imx_sip.h>
 #include <linux/arm-smccc.h>
+#include <linux/delay.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -90,11 +91,28 @@ int board_early_init_f(void)
 }
 
 #if IS_ENABLED(CONFIG_FEC_MXC)
-static int setup_fec(void)
+
+#define FEC_RST_PAD IMX_GPIO_NR(1, 5)
+static iomux_v3_cfg_t const fec1_rst_pads[] = {
+	        IMX8MM_PAD_GPIO1_IO05_GPIO1_IO5 | MUX_PAD_CTRL(NO_PAD_CTRL),
+};
+static void setup_iomux_fec(void)
+{
+	imx_iomux_v3_setup_multiple_pads(fec1_rst_pads,ARRAY_SIZE(fec1_rst_pads));
+	gpio_request(FEC_RST_PAD, "fec1_rst");
+	gpio_direction_output(FEC_RST_PAD, 1);
+	mdelay(10);
+	gpio_direction_output(FEC_RST_PAD, 0);
+	mdelay(20);
+	gpio_direction_output(FEC_RST_PAD, 1);
+	mdelay(20);
+}
+
+ int setup_fec(void)
 {
 	struct iomuxc_gpr_base_regs *gpr =
 		(struct iomuxc_gpr_base_regs *)IOMUXC_GPR_BASE_ADDR;
-
+	setup_iomux_fec();
 	/* Use 125M anatop REF_CLK1 for ENET1, not from external */
 	clrsetbits_le32(&gpr->gpr[1], 0x2000, 0);
 
